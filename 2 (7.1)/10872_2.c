489ea02cd4e3c4961222255920903237661406d4
virture-04 <75198682+virture-04@users.noreply.github.com>
2024-07-01 18:24:35 +0900

    10872_2 제출

---
+++ 2 (7.1)/10872_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int factorial(int n){
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%d", factorial(n));
+}
